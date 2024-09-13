9736a54dfe2313cd5b116a5205c251c7489c719b
NikosSgo <nikosgonnik@gmail.com>
2024-09-13 21:35:23 +0300

    A method for finding the sum of all divisors of a number that are coprime with the sum of the digits of the number and not coprime with the product of the digits of the number.

--- IntroductionToTheRuby/MethodsForWorkWithNumbers.rb
+++ IntroductionToTheRuby/MethodsForWorkWithNumbers.rb
@@ -14,5 +14,19 @@ def MinNotAnEvenDigit(number)
 end
 
 
+#Метод 3 Найти сумму всех делителей числа, взаимно простых с суммой
+#цифр числа и не взаимно простых с произведением цифр числа.
 
+def SumDevisors(number)
+	sumDigits =  0
+	productDigits = 1
+	digits = number.to_s.split("").map(&:to_i).each do |x|
+		sumDigits += x
+		productDigits *= x
+	end
+	puts "Сумма - #{sumDigits}, произведение - #{productDigits}"
+	sumDevisors = 0
+	devisors = (1..number).select { |devisor| number%devisor == 0 and devisor.gcd(sumDigits) == 1 and devisor.gcd(productDigits) != 1}.each { |x| sumDevisors+= x}
+	return sumDevisors
+end
 
