b657079f54400beca156c0eddd967b1ed2ed7882
NikosSgo <nikosgonnik@gmail.com>
2024-09-13 19:09:43 +0300

    Greeting with the user

---
+++ IntroductionToTheRuby/GreetingWithTheUser.rb
@@ -0,0 +1,45 @@
+#2)Поздороваться с пользователем с использованием форматирования
+#строки. Спросить какой язык у пользователя любимый, в случае, если это
+#ruby, ответить что пользователь подлиза, иначе обязательно ответить, что
+#скоро будет ruby и поставить различные комментарии для нескольких
+#языков.
+name =  "Пользователь"
+if ARGV.length > 0
+	name = ARGV[0]
+end
+
+ARGV.clear
+
+puts "Привет, #{name}!\n "
+
+languages = [
+  "Ruby", "Python", "JavaScript", "Java", "C++", "C#", "Go", "Rust", "Swift", "Kotlin", 
+  "PHP", "TypeScript", "Perl", "Scala", "Elixir", "Haskell", "C", "Objective-C", "Dart", 
+  "R", "MATLAB", "Lua", "Visual Basic", "Fortran", "COBOL", "F#", "Erlang", "Groovy", 
+  "Scheme", "Prolog", "Julia", "Crystal", "VimL", "Tcl", "Smalltalk", "Ada", "Pascal", 
+  "Racket", "OCaml", "Nim", "Awk", "SAS", "ABAP", "APL", "Lisp", "Xojo", "Hack", 
+  "SML (Standard ML)", "Pony", "Red", "Zig", "ActionScript", "Q#", "Scratch", "Bash", 
+  "PowerShell", "Delphi", "Forth", "Mercury", "Modula-2", "REXX", "Eiffel", "Chapel", 
+  "Fantom", "Monkey C", "Wolfram", "Logo", "J", "Kotlin Native", "VHDL", "Verilog"
+]
+
+puts "#{name}, какой у тебя любимый язык программирования?"
+
+favouriteLanguage = gets.chomp
+
+case favouriteLanguage.downcase
+when "ruby"
+	puts "#{name}, ты подлиза!"
+when "fortran"
+	puts "#{name}, тебе пора на пенсию."
+when "kotlin"
+	puts "#{name}, ты фанат мобилок!"
+when "pascal"
+	puts "#{name}, ты ничего не перепутал?"
+else
+	if languages.include?(favouriteLanguage)
+		puts "#{name}, хороший выбор, но скоро ты познакомишься с Ruby!"
+	else 
+		puts "Не слышал про такой язык. Тебе следует познакомиться с Ruby!"
+	end
+end
