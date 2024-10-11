9ad6b989c06df6082f8041877aea13f751a867d4
NikosSgo <nikosgonnik@gmail.com>
2024-10-11 23:31:45 +0300

    Lab 2/Exercise 1/Task 7. Tasks 3-6 already done. Add validate method for phone and add validate in phone setter.

--- Students/StudentClass.rb
+++ Students/StudentClass.rb
@@ -1,25 +1,39 @@
 class Student
-	attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+  attr_accessor :id, :surname, :name, :patronymic, :telegram, :email, :git
+  
+  attr_reader :phone
 
-	def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-		@id = id
-		@surname = surname
-		@name = name
-		@patronymic = patronymic
-		@phone = phone
-		@telegram = telegram
-		@email = email
-		@git = git
-	end
+  def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    @id = id
+    @surname = surname
+    @name = name
+    @patronymic = patronymic
+    self.phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
+  end
 
-	def to_s
-		str = "surname: #{@surname}, name: #{@name}, patronymic: #{@patronymic}"
-		str+=",id: #{@id}" if @id
-		str+=",phone: #{@phone}" if @phone
-		str+=",telegram: #{@telegram}" if @telegram
-		str+=",email: #{@email}" if @email
-		str+=",git: #{@git}" if @git 
-		return str
-	end
-	
+  def to_s
+    str = "surname: #{@surname}, name: #{@name}, patronymic: #{@patronymic}"
+    str += ", id: #{@id}" if @id
+    str += ", phone: #{@phone}" if @phone
+    str += ", telegram: #{@telegram}" if @telegram
+    str += ", email: #{@email}" if @email
+    str += ", git: #{@git}" if @git
+    return str
+  end
+
+  def self.valid_phone?(phone)
+    phone =~ /\A(\+7|8)?\s?(\(?\d{3}\)?[\s-]?)?[\d\s-]{7,10}\z/
+  end
+
+  def phone=(phone)
+    if !phone.nil? && self.class.valid_phone?(phone)
+	  puts "#{phone.nil?}"
+      @phone = phone
+    else
+      raise ArgumentError, "Неверный формат телефонного номера"
+    end
+  end
 end
--- Students/main.rb
+++ Students/main.rb
@@ -1,7 +1,7 @@
 require_relative "StudentClass"
 
-first_student = Student.new(surname: "Сгонниик", name: "Николай", patronymic: "Сергеевич",git: "https://github.com/NikosSgo")
-second_student = Student.new(surname: "Григорегко", name: "Никита", patronymic: "Алексеевич")
+first_student = Student.new(surname: "Сгонниик", name: "Николай", patronymic: "Сергеевич",git: "https://github.com/NikosSgo", phone: "89286341261")
+second_student = Student.new(surname: "Григоренко", name: "Никита", patronymic: "Алексеевич", phone: "89286313261")
 
 puts "Данные первого студента : #{first_student}."
 puts "Данные второго студента :#{second_student}."
