500eb234099b1f05eac1850cff37738995f3a667
NikosSgo <nikosgonnik@gmail.com>
2024-10-18 19:14:23 +0300

    Lab 3/Exercise 1. Method that swaps the maximum and minimum elements of an array

--- Polymorphism, blocks, collections and tests/Methods_for_processing_arrays_without_using_loops.rb
+++ Polymorphism, blocks, collections and tests/Methods_for_processing_arrays_without_using_loops.rb
@@ -2,3 +2,17 @@ def is_this_element_global_minimum?(array,index)
   return array[index] == array.min
 end
 
+def swap_min_and_max_el(array)
+  min = array.min
+  max = array.max
+  array.map do |el|
+    if el == max
+	  el = min
+	elsif el == min
+	  el = max
+	end
+	el
+  end
+end
+	
+
