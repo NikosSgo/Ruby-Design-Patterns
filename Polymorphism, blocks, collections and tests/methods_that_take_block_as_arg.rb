8f556f96f090717c03973089c4db5947e51d566e
NikosSgo <nikosgonnik@gmail.com>
2024-10-25 22:13:37 +0300

    Lab 3/ Exercise 2. Implemented my own version of the method cycle

--- Polymorphism, blocks, collections and tests/methods_that_take_block_as_arg.rb
+++ Polymorphism, blocks, collections and tests/methods_that_take_block_as_arg.rb
@@ -6,4 +6,14 @@ class MyArray
     @array = array
   end
   
+  def my_cycle(rep = nil)
+    cycle = 0
+    while(cycle != rep)
+      for i in 0...@array.length do 
+	    yield(@array[i])
+	  end
+	  cycle+=1
+    end
+  end
+  
 end
