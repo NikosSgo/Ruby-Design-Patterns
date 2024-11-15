d35f1577b9ba9722d3cc189e55fe92759c1037c7
NikosSgo <nikosgonnik@gmail.com>
2024-11-15 22:50:42 +0300

    Lab 4/ Exercise 1/ Task 2. Added methods for getting data table elements

--- Class models/data_table.rb
+++ Class models/data_table.rb
@@ -2,4 +2,16 @@ class DataTable
   def initialize(data)
     @data = data.map(&:freeze).freeze
   end
+
+  def get_element(row, col)
+    @data[row][col]
+  end
+
+  def row_count
+    @data.size
+  end
+
+  def column_count
+    @data.empty? ? 0 : @data[0].size
+  end
 end
